3c975e32802489af39dc7eb08e9cdf1263dcd59b
Sergey <anaxronik@gmail.com>
2020-01-23 07:11:12 +0500

    Добавил модификацию задач

--- todoapp/tasks/urls.py
+++ todoapp/tasks/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='list'),
     path('create/', views.create_task, name='create_task'),
     path('delete/<int:uid>', views.delete, name='delete_task'),
-
+    path('modify/<int:uid>', views.modify, name='modify_task'),
 ]
--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
-    path('', include('tasks.urls')),
 ]
